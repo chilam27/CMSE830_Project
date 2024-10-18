c66aff13d6586f0b8274ac665d79bdb34c60ce3e
Dao Chi Lam <66649712+chilam27@users.noreply.github.com>
2024-10-18 12:43:54 -0400

    Update Price Prediction.py

--- StreamlitApp/pages/Price Prediction.py
+++ StreamlitApp/pages/Price Prediction.py
@@ -3,7 +3,6 @@ import streamlit as st
 import altair as alt
 import pandas as pd
 import numpy as np
-import seaborn as sns
 from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import StandardScaler
 
