ad5acd08d3a096673d90856d66559149280245d7
Dao Chi Lam <66649712+chilam27@users.noreply.github.com>
2024-12-05 03:37:29 -0500

    Add files via upload

---
+++ StreamlitApp/pages/Underlying Workflow.py
@@ -0,0 +1,21 @@
+# Import libraries
+import streamlit as st
+import pandas as pd
+import altair as alt
+import streamlit.components.v1 as components
+
+st.set_page_config(page_title="Underlying Methodology", 
+                   page_icon="🔬",
+                   layout="wide")
+
+st.markdown("# Underlying Methodology")
+
+# display notebook as html
+path_to_html = "./CMSE830_Project.html" 
+
+with open(path_to_html,'r') as f: 
+    html_data = f.read()
+
+# Show in webpage
+with st.expander("Workflow Notebook (HTML)"):
+    st.components.v1.html(html_data, scrolling=True, height=800)
