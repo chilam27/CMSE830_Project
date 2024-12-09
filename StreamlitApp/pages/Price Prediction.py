addd8b4b2c68a839e434018aa7e61acec71b84ba
Dao Chi Lam <66649712+chilam27@users.noreply.github.com>
2024-12-08 23:32:25 -0500

    Update Price Prediction.py

--- StreamlitApp/pages/Price Prediction.py
+++ StreamlitApp/pages/Price Prediction.py
@@ -75,16 +75,16 @@ def prediction(CompareMedianSalePrice, Area, Bed, Bath, ModelChoice):
     area_df = trulia_df[['Area_Encoded','Area']].groupby('Area').median()
     Area = area_df.loc[Area]
 
-    st.write(Area)
-    st.write(Bed[0])
-    st.write(Bath[0])
+    # st.write(Area)
+    # st.write(Bed[0])
+    # st.write(Bath[0])
   
     if ModelChoice[0] == "Linear Regression":
-        prediction = linear.predict([[CompareMedianSalePrice, Area, Bed[0], Bath[0]]])
+        prediction = linear.predict([[CompareMedianSalePrice, Area[0], Bed[0], Bath[0]]])
     elif ModelChoice[0] == "Lasso Regression":
-        prediction = lasso.predict([[CompareMedianSalePrice, Area, Bath[0], Bed[0]]])
+        prediction = lasso.predict([[CompareMedianSalePrice, Area[0], Bath[0], Bed[0]]])
     elif ModelChoice[0] == "XGBoost Regressor":
-        prediction = xgboost.predict([[CompareMedianSalePrice, Area, Bath[0], Bed[0]]])
+        prediction = xgboost.predict([[CompareMedianSalePrice, Area[0], Bath[0], Bed[0]]])
     # elif ModelChoice[0] == "Gradient Boosting Regressor":
     #     prediction = gradient.predict([[CompareMedianSalePrice, Area, Bath[0], Bed[0]]])
     # else:
