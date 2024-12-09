dc21e44ba19d2680692f281ba787b4a5d7915521
Dao Chi Lam <66649712+chilam27@users.noreply.github.com>
2024-12-09 00:20:41 -0500

    Update Housing Specification.py

--- StreamlitApp/pages/Housing Specification.py
+++ StreamlitApp/pages/Housing Specification.py
@@ -20,52 +20,29 @@ st.write(
     """The plots and figures in this section are generated from the processed Trulia and Census datasets. We can use the available tools to understand the trends in property sale prices based on their features."""
 )
 
-# st.sidebar.header("Processed Dataset Summary")
-
-# st.sidebar.markdown(
-#     """### Trulia Dataset Size
-# - Rows: 2659
-# - Columns: 13
-
-# ### Trulia Variables
-# - *Sale*: int; sale price of the house
-# - *Area*: object; the area that the house is located at
-# - *Bed*: float; number of bedrooms in a house
-# - *Bath*: float; number of bathrooms in a house
-# - *Crime_Rate*: int; crime rate from 1-5
-# - *Car_Commute_Percentage*: float; proportion of people in the area commute by car
-# - *Elemenatary_School*: int; number of elementary schools nearby  
-# - *Middle_School*: int; number of middle schools nearby  
-# - *High_School*: int; number of high schools nearby
-# - *Total_School*: int; number of schools nearby
-# - *Restaurant*: int; number of restaurants nearby
-# - *Grocery*: int; number of grocery stores nearby
-# - *Nightlife*: int; number of nightlife activities nearby
-
-# ### Census Dataset Size
-# - Rows: 24
-# - Columns: 10
-
-# ### Census Variables
-# - *Median Household Income*: float; median household income by neighborhood
-# - *Median Age*: float; median age by neighborhood
-# - *Total Population*: int; total population by neighborhood
-# - *Occupied*: int; number of occupancy by neighborhood
-# - *Vacant*: int; number of vacancy by neighborhood
-# - *Work Commute Short*: float; number of people commute < 15 minutes to work
-# - *Work Commute Average*: float; number of people commute 15-45 minutes to work
-# - *Work Commute Long*: float; number of people commute > 45 minutes to work
-# """)
-
-
 # import dataset
 trulia_df = pd.read_csv('StreamlitApp/final_df.csv')
 census_cleanned = pd.read_csv('StreamlitApp/census_df.csv')
 sale_stats = pd.read_csv('StreamlitApp/sale_stats.csv')
 
+st.sidebar.header("Neighborhood Filter")
+
+# set filter
+cat_list = np.sort(trulia_df['Area'].unique())
+val = [None]* len(cat_list)
+for i, cat in enumerate(cat_list):
+    val[i] = st.sidebar.checkbox(str(cat), value=True)
+
+# filter data based on selection
+trulia_filter_df = trulia_df[trulia_df['Area'].isin(cat_list[val])].reset_index(drop = True)
+if trulia_filter_df.shape[0] > 0:
+    st.dataframe(trulia_filter_df)
+else:
+    st.write("Empty Dataframe")
+
 # display dataset
 with st.expander("View Trulia processed dataset"):
-  st.dataframe(trulia_df, height = 250)
+  st.dataframe(trulia_filter_df, height = 250)
   st.write("""
   ### Trulia Dataset Size
   - Rows: 2659
@@ -108,7 +85,7 @@ with st.expander("View Census Processed dataset"):
 st.divider()
 
 # "Sale" distribution plot
-fig = px.histogram(trulia_df, x="Sale")
+fig = px.histogram(trulia_filter_df, x="Sale")
 fig.update_layout(title='Sale Distribtuion', width=1100, height=500)
 st.plotly_chart(fig)
 
@@ -119,7 +96,7 @@ with st.expander("Sale variable basic summary statistics"):
 st.divider()
 
 # correlation heatmap
-trulia_num_df = trulia_df.select_dtypes(include = np.number)
+trulia_num_df = trulia_filter_df.select_dtypes(include = np.number)
 corr_matrix = trulia_num_df.corr()
 
 # create the heatmap
@@ -140,14 +117,14 @@ tab1, tab2= st.tabs(["📦 Box Plot", "🗺️ Map Plot"])
 
 with tab1:
     # box plot
-    fig = px.box(trulia_df, x="Area", y="Sale", color = "Area")
+    fig = px.box(trulia_filter_df, x="Area", y="Sale", color = "Area")
     fig.update_layout(title='Sale Box Plot Grouped by Neighborhood', width=1100, height=800, showlegend=False)
     st.plotly_chart(fig)
 
 with tab2:
     # map plot
     census_cleanned['Geometry'] = census_cleanned['Geometry'].apply(wkt.loads)
-    median_sale_area = trulia_df[['Area', 'Sale']].groupby(['Area'], as_index = False).median()
+    median_sale_area = trulia_filter_df[['Area', 'Sale']].groupby(['Area'], as_index = False).median()
     geo_df = pd.merge(census_cleanned, median_sale_area, 'right', on = 'Area').set_index('Area')
     geo_df = gpd.GeoDataFrame(geo_df, geometry = 'Geometry')
     
@@ -171,13 +148,13 @@ with tab2:
 st.divider()
 
 # "Sale" vs "Bath" scatter plot plot
-fig = px.scatter(trulia_df, x="Bath", y="Sale", trendline="ols", trendline_color_override = "white")
+fig = px.scatter(trulia_filter_df, x="Bath", y="Sale", trendline="ols", trendline_color_override = "white")
 fig.update_layout(title='Sale vs. Bath Scatter Plot', width=1100, height=500)
 st.plotly_chart(fig)
 
 st.divider()
 
 # "Sale" vs "Bed" scatter plot plot
-fig = px.scatter(trulia_df, x="Bed", y="Sale", trendline="ols", trendline_color_override = "white")
+fig = px.scatter(trulia_filter_df, x="Bed", y="Sale", trendline="ols", trendline_color_override = "white")
 fig.update_layout(title='Sale vs. Bed Scatter Plot', width=1100, height=500)
 st.plotly_chart(fig)
