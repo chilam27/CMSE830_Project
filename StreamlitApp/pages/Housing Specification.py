62ae94f77f27a21df5e4f0fcc0b0cbf576d7a127
Dao Chi Lam <66649712+chilam27@users.noreply.github.com>
2024-12-09 01:05:07 -0500

    Update Housing Specification.py

--- StreamlitApp/pages/Housing Specification.py
+++ StreamlitApp/pages/Housing Specification.py
@@ -159,8 +159,8 @@ with tab2:
 st.divider()
 
 # "Sale" vs "Bath" scatter plot plot
-fig = px.box(trulia_filter_df, x = 'Bath', y = 'Sale')
-fig.add_trace(px.scatter(trulia_filter_df, x = 'Bath', y = 'Sale', trendline = "ols").data[0])
+fig = px.box(trulia_filter_df, x = 'Bath', y = 'Sale',  trendline = "ols")
+# fig.add_trace(px.scatter(trulia_filter_df, x = 'Bath', y = 'Sale', trendline = "ols").data[0])
 fig.update_layout(title = 'Sale vs. Bath Scatter Plot', width = 1100, height = 500)
 st.plotly_chart(fig)
 
