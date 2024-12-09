5f99f5dfeca25e6441bca39c1e9d6e04e5972473
Dao Chi Lam <66649712+chilam27@users.noreply.github.com>
2024-12-09 01:22:16 -0500

    Update Home.py

--- StreamlitApp/Home.py
+++ StreamlitApp/Home.py
@@ -26,19 +26,21 @@ st.write(
 - What is the current state of the housing market?
 - What are the features that have high effect on to the rent of a property?
 - How good is our prediction for the rent of the property?
-"""
-)
 
-st.markdown("## Project Outline")
+## Project Outline
 
-st.write(
-    """The following are the outline steps that I will be performing: <u>text</u>
+The following are the outline steps that I will be performing: <u>text</u>
 
 - Data collection: use BeautifulSoup to scrape property data from Trulia, a popular real estate website. Gather all listed variables that can be used for data analysis; download Census data through its API, a provider of data on the National, State, and Local levels that help define who we are as a nation and the makeup of the people that live in this country; download housing market data that is available on Redfin, a real estate company that offers services for buying, selling, renting, and financing homes.
 - Data cleaning: read in data and prepare it for data analysis; steps include: tidy up the categorical features, deal with null value, etc.
 - Exploratory data analysis (EDA): examine the cleaned data and its trends through Streamlit.
 - Model building: determine which model (Linear, Lasso, XGBoost) work best (that return the smallest error) and tune the model with different parameters using GridSearchCV.
 - Deploy Application: save all relevant files to a [GitHub repository](https://github.com/chilam27/CMSE830_Project?tab=readme-ov-file) and deploy the application onto Streamlit Community Cloud."
+
+## Dataset
+- Trulia dataset: the Boston housing data scraped from [Trulia](https://www.trulia.com/). We used the BeautifulSoup package to extract information from the website for educational purposes only. The dataset includes variables such as sale, address, area, bed, bath, school, crime, commute, shop_eat, description, feature, and URL.
+- Census dataset: this is the Census dataset that we requested through its API. We selected a few variables that we think might help us understand the real estate market as well as aiding us in predicting the housing prices. Some of the useful variables include median household income, median age, total population, occupied, vacant, and work commute duration.
+- Redfin dataset: the Boston housing market data collected from [Redfin](https://www.redfin.com/), a real estate company that helps people buy, sell, and rent homes. The data is a combination of four separate datasets and includes information such as homes sold, active listings, median sale price, and median sale price per square foot (PPSF). By understanding the past and current state of the housing market, we can make better decisions regarding buying and selling properties.
 """
 )
 
