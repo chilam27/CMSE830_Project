87e71f6368dae1aac7e569f901892638692cdaae
Dao Chi Lam <66649712+chilam27@users.noreply.github.com>
2024-12-09 02:15:21 -0500

    Update Underlying Workflow.py

--- StreamlitApp/pages/Underlying Workflow.py
+++ StreamlitApp/pages/Underlying Workflow.py
@@ -10,6 +10,21 @@ st.set_page_config(page_title="Underlying Methodology",
 
 st.markdown("# 🔬 Underlying Methodology")
 
+## Project Outline
+st.write("""
+## Project Outline
+
+The following are the outline steps that I will be performing: <u>text</u>
+
+- Data collection: use BeautifulSoup to scrape property data from Trulia, a popular real estate website. Gather all listed variables that can be used for data analysis; download Census data through its API, a provider of data on the National, State, and Local levels that help define who we are as a nation and the makeup of the people that live in this country; download housing market data that is available on Redfin, a real estate company that offers services for buying, selling, renting, and financing homes.
+- Data cleaning: read in data and prepare it for data analysis; steps include: tidy up the categorical features, deal with null value, etc.
+- Exploratory data analysis (EDA): examine the cleaned data and its trends through Streamlit.
+- Model building: determine which model (Linear, Lasso, XGBoost) work best (that return the smallest error) and tune the model with different parameters using GridSearchCV.
+- Deploy Application: save all relevant files to a [GitHub repository](https://github.com/chilam27/CMSE830_Project?tab=readme-ov-file) and deploy the application onto Streamlit Community Cloud."
+
+## Underlying Workflow Notebook
+""")
+
 # display notebook as html
 path_to_html = "StreamlitApp/CMSE830_project.html"
 
